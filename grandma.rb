75c96767e15bae609a01b2cae2919304ef72271a
Alice Brunel <alc.brunel@gmail.com>
2019-07-06 12:20:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(string)
   if string == string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    return ""
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
